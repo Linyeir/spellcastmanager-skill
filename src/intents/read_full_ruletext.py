694a75fe4d9b2ad63fe82e9353b7e73b23182cbb
RobinRother <robin.rother@web.de>
2021-11-26 17:52:46 +0100

    implement basic api wrapper and finalize full ruletext intent

--- src/intents/read_full_ruletext.py
+++ src/intents/read_full_ruletext.py
@@ -1,6 +1,19 @@
+from ..backend import spell_api_wrapper as spell_api
+
+"""
+ actually handles the full ruletext intent
+ - the api is queried with the extracted spellname
+ - the replied ruletext is spoken aloud
+ - if the spell s not found or the api threw an error, the fallback is read instead
+"""
 def _read_full_ruletext(self, message):
-        spell = message.data.get('spell')
-        if spell is not None:
-            self.speak_dialog('long.ruletext', {'spell': spell})
+        spell_name = message.data.get('spell')
+
+        if spell_name is not None:
+            ruletext = spell_api.get_full_ruletext(spell_name)
+            if ruletext != 'empty':
+                self.speak_dialog('long.ruletext', {'ruletext': ruletext})
+            else:
+                self.speak_dialog('ruletext.fallback')
         else:
             self.speak_dialog('ruletext.fallback')
