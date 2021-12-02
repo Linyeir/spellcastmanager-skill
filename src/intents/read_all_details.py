49baf2d0e09aef2737768a68413b2cacad4e024b
Robin Rother <robin.rother@web.de>
2021-12-02 09:32:47 +0100

    adjust to new api wrapper

--- src/intents/read_all_details.py
+++ src/intents/read_all_details.py
@@ -1,26 +1,27 @@
-from ..backend import spell_api_wrapper
+from ..backend.spell_api_wrapper import Spell_api_wrapper
 
 def _read_all_details(self, message):
     spell_name_input = message.data.get('spellname')
+    spell = Spell_api_wrapper(self, spell_name_input)
     if spell_name_input is not None:
-        spell_name = self.spell_api.get_detail()    #######
+        spell_name = spell.get_detail()    #######
         if spell_name == 'empty':
             self.speak_dialog('ruletext.invalid.spell', {'spellname': spell_name_input})
         else:
-            spell_range = self.spell_api.get_detail()
-            spell_components = self.spell_api.get_detail()
-            spell_material = self.spell_api.get_detail()
-            spell_ritual = self.spell_api.get_detail()
-            spell_duration = self.spell_api.get_detail()
-            spell_concentration = self.spell_api.get_detail()
-            spell_castingTime = self.spell_api.get_detail()
-            spell_level = self.spell_api.get_detail()
-            spell_damageType = self.spell_api.get_detail()
-            spell_damage = self.spell_api.get_detail()          ### automatic lowest level besides user is asking?!?!?!
-            spell_savingThrow = self.spell_api.get_detail()
-            spell_areaOfEffectType = self.spell_api.get_detail()
-            spell_size = self.spell_api.get_detail()
-            spell_school = self.spell_api.get_detail()
+            spell_range = spell.get_detail('range')
+            spell_components = spell.get_detail('components')
+            spell_material = spell.get_detail('material')
+            spell_ritual = spell.get_detail('ritual')
+            spell_duration = spell.get_detail('duration')
+            spell_concentration = spell.get_detail('concentration')
+            spell_castingTime = spell.get_detail('casting_time')
+            spell_level = spell.get_detail('level')
+            spell_damageType = spell.get_detail('desc')
+            spell_damage = spell.get_detail('damage')                           # automatic lowest level besides user is asking?, Damage type?
+            spell_savingThrow = spell.get_detail('school')                      # dc_type, dc_success
+            spell_areaOfEffectType = spell.get_detail('area_of_effect[type]')   # not implemented yet
+            spell_size = spell.get_detail('area_of_effect[size]')               # not implemented yet
+            spell_school = spell.get_detail('desc')
     else:
         self.speak_dialog('ruletext.fallback')
 
