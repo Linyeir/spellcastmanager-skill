928c2d808744dcceee5c6d35a125435ec6d316e5
Alexandra Schmalz <inf3461@hs-worms.de>
2021-12-11 22:41:36 +0100

    continuing slot logic

--- src/backend/response_builder.py
+++ src/backend/response_builder.py
@@ -1,6 +1,6 @@
 from requests.models import Response
 from Spell_class import Spell_class
-
+'''
 # for get_single_detail intent
 # for read_full_ruletext intent
 def build_response_get_single_detail(spell_name_in, spell_detail_in, slot_level = 0):
@@ -15,13 +15,18 @@ def build_response_get_single_detail(spell_name_in, spell_detail_in, slot_level 
         # slot too high or max -> max
         # slot inbetween -> specified slot
 
-        if 
-        
+        if detail is spell.damage_at_slot_level or detail is spell.damage_at_character_level or detail is spell.heal_at_slot_level or detail is spell.heal_at_character_level:
+            first_key = list(detail())[0]
+            if slot_level <= first_key:
+                slot_detail = ''
+                if slot_level < first_key:
+                    slot_detail = 'The stated spell slot is to low. You need at least a spell slot of ' + first_key + '\n'
+                slot_detail = slot_detail + ''
         return detail
 
         # logic for slot level not implemented yet 
 
-    
+ '''   
 
 
 def build_response_all_details(spell_name_in):
@@ -101,4 +106,7 @@ def build_response_all_details(spell_name_in):
         if spell.school is not 'empty':
             response = response + 'School ' + spell.school + '\n'
 
+
         return response 
+
+print(build_response_all_details("fireball"))
