16528660511e5d9cd604767fc6d540827aaaaf46
Robin Rother <robin.rother@outlook.de>
2021-12-15 15:55:59 +0100

    add exception classes

--- src/response_builders/get_spell_description.py
+++ src/response_builders/get_spell_description.py
@@ -2,21 +2,12 @@
 from response_builder_base import ResponseBuilderBase
 from ..utils.Spell_class import Spell_class
 
+
 class ResponseBuilderGetSpellDescription(ResponseBuilderBase):
 
-    def get_response(detail: str, casting_level: str) -> dict:
-        
-        return super().get_response(casting_level)
+    def __init__(self, spell_name: str):
+        self._spell = Spell_class(spell_name)
 
-    def build_full_ruletext(spell_name_in):
-        spell = Spell_class(spell_name_in)
-        response = ''
-
-        if spell.name == 'empty':
-            return 'no spell'
-        else:
-            if spell.desc != 'empty':
-                response = spell.desc
-            else:
-                response = 'No description found'
+    def get_response(self, detail: str, casting_level: str) -> dict:
+        response = self._spell.desc
         return response
