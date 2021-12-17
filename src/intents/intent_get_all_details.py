c0dd33f22be0fc5e70d0f452fa5f8c844286fb37
AlexandraSchmalz <alexandra.schmalz@outlook.com>
2021-12-17 12:14:11 +0100

    implement intentGetSingleDetail (without call dialog method)

--- src/intents/intent_get_all_details.py
+++ src/intents/intent_get_all_details.py
@@ -7,14 +7,7 @@ from ..utils.exceptions.invalid_spell_error import InvalidSpellError
 
 class IntentGetAllDetails(IntentBase):
     def __init__(self):
-        pass
-
-    #def _extract_casting_level(message):
-    #    casting_level_input = message.data.get('casting_level')
-    #    if casting_level_input == None:
-    #        casting_level_input == 'default'
-    #    return casting_level_input
-        
+        pass        
 
     def execute(self, Spellcastmanager, message):
         try:
@@ -35,40 +28,6 @@ class IntentGetAllDetails(IntentBase):
         else:
             dialog = 'get.all.details.category.' + str(spell_category)
             Spellcastmanager.speak_dialog(dialog, response)
-
-
-
-
-
-
-
-          
-            # Spellcastmanager.speak_dialog('get.all.details.dialog', {'name': response['name'], 
-            #                                                                 'desc': response['desc'], 
-            #                                                                 'higher_level': response['higher_level'],
-            #     -not complete! see list below! (casting level)              'range': response['range'],
-            #                                                                 'components': response['components'],
-            #                                                                 'material': response['material'],
-            #                                                                 'ritual': response['ritual'],
-            #                                                                 'duration': response['duration'],
-            #                                                                 'concentration': response['concentration'],
-            #                                                                 'casting_time': response['casting_time'],
-            #                                                                 'level': response['level'],
-            #                                                                 'attack_type': response['attack_type'],
-            #                                                                 'damage_type': response['damage_type'],
-            #                                                                 'damage_at_slot_level': response['damage_at_slot_level'],
-            #                                                                 'heal_at_slot_level': response['heal_at_slot_level'],
-            #                                                                 'damage_at_character_level': response['damage_at_character_level'],
-            #                                                                 'heal_at_character_level': response['heal_at_character_level'],
-            #                                                                 'dc_type': response['dc_type'],
-            #                                                                 'dc_success': response['dc_success'],
-            #                                                                 'area_of_effect_type': response['area_of_effect_type'],
-            #                                                                 'area_of_effect_size': response['area_of_effect_size'],
-            #                                                                 'school': response['school']
-            #                                                                 }) 
-
-
-
 
 # name
 # desc
---
+++ src/intents/intent_get_single_detail.py
@@ -0,0 +1,52 @@
+from .intent_base import IntentBase
+from ..response_builders.response_builder_get_single_detail import ResponseBuilderGetSingleDetail
+from ..utils.exceptions.api_not_reachable_error import APINotReachableError
+from ..utils.exceptions.no_spell_specified_error import NoSpellSpecifiedError
+from ..utils.exceptions.invalid_spell_error import InvalidSpellError
+from ..utils.exceptions.no_detail_specified_error import NoDetailSpecifiedError
+from ..utils.exceptions.invalid_detail_error import InvalidDetailError
+
+
+class IntentGetSingleDetail(IntentBase):
+    def __init__(self):
+        pass
+
+    def _extract_detail(message):
+        detail_input = message.data.get('detail')
+        if detail_input == None:
+            raise NoDetailSpecifiedError
+        return detail_input
+
+    def _extract_casting_level(message):
+       casting_level_input = message.data.get('casting_level')
+       if casting_level_input == None:
+           casting_level_input == 'default'
+       return casting_level_input
+
+    def _call_detail_dialog():
+        pass
+
+    def execute(self, Spellcastmanager, message):
+        try:
+            spell_name_input = super()._extract_spell_name(message)
+            detail_input = self._extract_detail(message)
+            casting_level_input = self._extract_casting_level(message)
+            self._response_builder = ResponseBuilderGetSingleDetail(spell_name_input)
+            response = self._response_builder.get_response(detail_input, casting_level_input)
+        except APINotReachableError as err:
+            Spellcastmanager.log.error(err)
+            Spellcastmanager.speak_dialog('api.not.reachable.error')
+        except NoSpellSpecifiedError as err:
+            Spellcastmanager.log.error(err)
+            Spellcastmanager.speak_dialog('no.spell.specified.error')
+        except InvalidSpellError as err:
+            Spellcastmanager.log.error(err)
+            Spellcastmanager.speak_dialog('invalid.spell.error', {'name': spell_name_input})
+        except NoDetailSpecifiedError as err:
+            Spellcastmanager.log.error(err)
+            Spellcastmanager.speak_dialog('no.detail.specified.error')
+        except InvalidDetailError as err:
+            Spellcastmanager.log.error(err)
+            Spellcastmanager.speak_dialog('invalid.detail.error', {'detail': detail_input})
+        else:
+            self._call_detail_dialog()
