6fd8a847732cdc024f8eebef921aa85bcdfbab3a
Robin Rother <robin.rother@outlook.de>
2022-02-05 21:02:21 +0100

    fix some stuff I guess

--- __init__.py
+++ __init__.py
@@ -10,7 +10,6 @@ from .src.intents.intent_get_help import IntentGetHelp
 from .src.intents.intent_change_settings import IntentChangeSettings
 
 
-
 class Spellcastmanager(MycroftSkill):
 
     # the constructor
@@ -25,21 +24,18 @@ class Spellcastmanager(MycroftSkill):
         self._language = self.settings.get('language')
         self._title = self.settings.get('title')
 
-    
     def set_settings(self):
         if self.settings.get('language') == "" or self.settings.get('title') == "":
-            self.speak_dialog('settings.empty')
             ics = IntentChangeSettings()
-            ics.execute_with_guide(self)
+            ics.execute_if_not_set(self)
             return self.settings.get('title')
 
-    #-----------------------------------------------------
+    # -----------------------------------------------------
     # handlers
 
     @intent_file_handler('spellcastmanager.intent')
     def handle_spellcastmanager(self, message):
         self.speak_dialog('spellcastmanager')
-
 
     @intent_handler('change.settings.intent')
     def handle_change_settings(self):
@@ -48,50 +44,49 @@ class Spellcastmanager(MycroftSkill):
 
     # reads the full ruletext to user
     @intent_handler(IntentBuilder('getSpellDescription')
-        .require('theSpell')
-        .one_of('ruletext', 'description', 'explanation', 'what', 'tellMeAbout')
-        .optionally('spellname'))
+                    .require('theSpell')
+                    .one_of('ruletext', 'description', 'explanation', 'what', 'tellMeAbout')
+                    .optionally('spellname'))
     def handle_get_spell_description(self, message):
         intent = IntentGetSpellDescription()
         intent.execute(self, message)
-        
 
     # reads all details of spell to user
+
     @intent_handler(IntentBuilder('getAllDetails')
-        .optionally('spellname')
-        .require('detail')
-        .require('theSpell'))
+                    .optionally('spellname')
+                    .require('detail')
+                    .require('theSpell'))
     def handle_get_all_details(self, message):
         intent = IntentGetAllDetails()
         intent.execute(self, message)
 
-
     # guides user through casting
+
     @intent_handler(IntentBuilder('invokeCastinAssistant')
-        .require('i')
-        .require('cast')
-        .require('theSpell')
-        .optionally('spellname'))
+                    .require('i')
+                    .require('cast')
+                    .require('theSpell')
+                    .optionally('spellname'))
     def handle_invoke_casting_assistant(self, message):
         intent = IntentInvokeCastingAssistant(self._language)
         intent.execute(self, message)
 
-
    # reads single detail of spell to user
+
     @intent_handler(IntentBuilder('getSingleDetail')
-        .optionally('spellname')
-        .require('theSpell')
-        .require('want')
-        .require('information')
-        )
+                    .optionally('spellname')
+                    .require('theSpell')
+                    .require('want')
+                    .require('information')
+                    )
     def handle_get_single_detail(self, message):
         intent = IntentGetSingleDetail()
         intent.execute(self, message)
 
-        
     # reads help to the user (padatious)
-    @intent_handler('get.help.intent')
 
+    @intent_handler('get.help.intent')
     def handle_get_help(self, message):
         intent = IntentGetHelp()
         intent.execute(self, message)
--- src/intents/intent_change_settings.py
+++ src/intents/intent_change_settings.py
@@ -1,21 +1,23 @@
 from .intent_base import IntentBase
 
+
 class IntentChangeSettings(IntentBase):
     def __init__(self):
         self._fail_message_setting = 'This setting does not exist. Choose either language or title'
         self._fail_message_value = 'You need to give me something to work with'
-        
 
     def execute(self, Spellcastmanager):
         """
         user calls intent themself
         """
         self._spellcastmanger = Spellcastmanager
-        self._setting_to_change = self._spellcastmanger.get_response('which.setting', validator=self._validate_chosen_setting, on_fail=self._fail_message_setting, num_retries=3)
+        self._setting_to_change = self._spellcastmanger.get_response(
+            'which.setting', validator=self._validate_chosen_setting, on_fail=self._fail_message_setting, num_retries=3)
         if self._setting_to_change == None:
             self._spellcastmanger.speak_dialog('too.many.fails')
             return
-        setting_value_input = self._spellcastmanger.get_response('which.setting.value', validator=self._validate_setting_value, on_fail=self._fail_message_value, num_retries=3)
+        setting_value_input = self._spellcastmanger.get_response(
+            'which.setting.value', validator=self._validate_setting_value, on_fail=self._fail_message_value, num_retries=3)
         if setting_value_input == None:
             self._spellcastmanger.speak_dialog('too.many.fails')
             return
@@ -23,26 +25,31 @@ class IntentChangeSettings(IntentBase):
             setting_value_input = 'en'
         self._spellcastmanger.settings[self._setting_to_change] = setting_value_input
         self._spellcastmanger.speak_dialog('alright')
-        
 
-    def execute_with_guide(self, Spellcastmanager):
+    def execute_if_not_set(self, Spellcastmanager):
         """
         spellcastmanager asks user to fill gaps
         """
+        self.speak_dialog('settings.empty')
         if Spellcastmanager.settings.get('language') == "":
-            setting_value_input = Spellcastmanager.get_response('which.language', validator=self._validate_setting_value, on_fail=self._fail_message_value, num_retries=3)
+            self._setting_to_change = 'language'
+            setting_value_input = Spellcastmanager.get_response(
+                'which.language', validator=self._validate_setting_value, on_fail=self._fail_message_value, num_retries=3)
             if setting_value_input == 'english':
                 setting_value_input = 'en'
             if setting_value_input == None:
-                Spellcastmanager.speak_dialog('assume.setting.value', {'setting_value': 'en'})
+                Spellcastmanager.speak_dialog(
+                    'assume.setting.value', {'setting_value': 'en'})
             Spellcastmanager.settings['language'] = 'en'
         if Spellcastmanager.settings.get('title') == "":
-            setting_value_input = Spellcastmanager.get_response('which.title', validator=self._validate_setting_value, on_fail=self._fail_message_value, num_retries=3)
+            self._setting_to_change = 'title'
+            setting_value_input = Spellcastmanager.get_response(
+                'which.title', validator=self._validate_setting_value, on_fail=self._fail_message_value, num_retries=3)
             if setting_value_input == None:
-                Spellcastmanager.speak_dialog('assume.setting.value', {'setting_value': 'oaf'})
+                Spellcastmanager.speak_dialog('assume.setting.value', {
+                                              'setting_value': 'oaf'})
                 setting_value_input = 'oaf'
             Spellcastmanager.settings['title'] = setting_value_input
-
 
     def _validate_chosen_setting(self, response):
         if self._spellcastmanger.settings.get(response, False) == False:
